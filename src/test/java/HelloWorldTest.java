09797ae2a217ee642423f826585eb6d879e14624
Irina-kiti <aliris2612@list.ru>
2024-05-31 17:06:15 +0300

    HelloWorldTest FirstTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.Test;
-
+import io.restassured.RestAssured;
+import  io.restassured.response.Response;
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello world");
+       Response response= RestAssured
+               .get("https://playground.learnqa.ru/api/hello")
+               .andReturn();
+       response.prettyPrint();
     }
 }
