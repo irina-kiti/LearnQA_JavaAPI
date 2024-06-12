4ed97107286b463bc84ad8544a2a4368550386fd
Irina-kiti <aliris2612@list.ru>
2024-06-12 19:46:44 +0300

    LongTime Ex8

--- src/test/java/longtimeJob.java
+++ src/test/java/longtimeJob.java
@@ -2,9 +2,11 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
+import java.util.concurrent.TimeUnit;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.lang.Thread.sleep;
 
 public class longtimeJob {
     @Test
@@ -16,19 +18,29 @@ public class longtimeJob {
                 .jsonPath();
 
         String responseToken = responseForToken.get("token");
-        int seconds = responseForToken.get("seconds");
+        int sec = responseForToken.get("sec");
 
         System.out.println(responseToken);
-        System.out.println(seconds);
+        System.out.println(sec);
 
         Response responseForStatus = RestAssured
                 .given()
-                .queryParam("token", "responseToken")
+                .queryParam("token", responseToken)
                 .get("https://playground.learnqa.ru/ajax/api/longtime_job")
                 .andReturn();
 
         responseForStatus.print();
         System.out.println(responseToken);
+        TimeUnit.SECONDS.sleep(sec);
+        /*Thread.sleep( sec*1000);*/
+
+        Response responseForResult = RestAssured
+                .given()
+                .queryParam("token", responseToken)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        responseForResult.print();
 
 
     }
